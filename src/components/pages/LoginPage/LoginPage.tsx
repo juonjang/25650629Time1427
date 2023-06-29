3778a9c8f4c08dc51a3a9cf97667fff858d44376
juon <juonjang@gmail.com>
2023-06-29 14:32:35 +0700

    creat-login-register

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,148 @@
+import { json } from "node:stream/consumers";
 import * as React from "react";
-import { useNavigate } from 'react-router-dom';
+import { Form, Navigate, useNavigate } from "react-router-dom";
+import { Formik, FormikProps } from "formik";
+import { Box, Stack, SxProps, TextField, Theme } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { ClassNames } from "@emotion/react";
 
 type LoginPageProps = {
   //
 };
 
-
 const LoginPage: React.FC<any> = () => {
   const navigate = useNavigate();
-  return (
-  <>
-  <h1>Login</h1>
-      <button onClick={() => navigate("/register")}>Register</button>
-  
-  </>
-  )
-};
+  const classes:SxProps<Theme> | any={
+    root:{display:'flex',justifyContent:"center"},
+    buttons:{marginTop:2}
+  }
 
+  const showFormV1 = ({
+    handleSubmit,
+    handleChange,
+    isSubmitting,
+    values,
+  }: FormikProps<any>) => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Username : </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={handleChange}
+          value={values.username}
+        />
+        <br />
+        <label htmlFor="">Password : </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+          value={values.password}
+        />
+        <br />
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+        <button onClick={() => navigate(-1)}>Back</button>
+      </form>
+    );
+  };
+  const showFormV2 = ({
+    handleSubmit,
+    handleChange,
+    isSubmitting,
+    values,
+  }: FormikProps<any>) => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          onChange={handleChange}
+          value={values.username}
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="password"
+          onChange={handleChange}
+          value={values.password}
+          type="password"
+
+        />
+        <br />
+        <Stack direction={"row"} spacing={2} sx={classes.buttons} >
+          <Button 
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={isSubmitting}
+          >
+            Login
+          </Button>
+          <Button
+            onClick={() => navigate("/Register")}
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={isSubmitting}
+          >
+            Register
+          </Button>
+        </Stack>
+
+      </form>
+    );
+  };
+
+  return (
+    <>
+      <Box sx={classes.root}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+          <Typography sx={{ fontSize: 20 ,justifyContent:"center",display:'flex'} } color="text.secondary" gutterBottom >
+              <h3>Login</h3>
+            </Typography>
+            <Formik
+              onSubmit={(values, { setSubmitting }) => {
+                alert(JSON.stringify(values));
+
+                setTimeout(() => {
+                  setSubmitting(false);
+                }, 2000);
+              }}
+              initialValues={{ username: "", password: "" }}
+            >
+              {(props) => showFormV2(props)}
+            </Formik>
+          </CardContent>
+        </Card>
+
+
+
+
+
+
+
+      </Box>
+    </>
+  );
+};
 
 export default LoginPage;
--- src/components/pages/RegisterPage/RegisterPage.tsx
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,14 @@
 import { json } from "node:stream/consumers";
 import * as React from "react";
-import { Form, useNavigate } from "react-router-dom";
+import { Form, Navigate, useNavigate } from "react-router-dom";
+import { Formik, FormikProps } from "formik";
+import { Box, Stack, SxProps, TextField, Theme } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { ClassNames } from "@emotion/react";
 
 type RegisterPageProps = {
   //
@@ -8,56 +16,133 @@ type RegisterPageProps = {
 
 const RegisterPage: React.FC<any> = () => {
   const navigate = useNavigate();
-  const [account , setAccount] = React.useState({username:' ', password:' ',})
+  const classes:SxProps<Theme> | any={
+    root:{display:'flex',justifyContent:"center"},
+    buttons:{marginTop:2}
+  }
+
+  const showFormV1 = ({
+    handleSubmit,
+    handleChange,
+    isSubmitting,
+    values,
+  }: FormikProps<any>) => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Username : </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={handleChange}
+          value={values.username}
+        />
+        <br />
+        <label htmlFor="">Password : </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+          value={values.password}
+        />
+        <br />
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+        <button onClick={() => navigate(-1)}>Back</button>
+      </form>
+    );
+  };
+  const showFormV2 = ({
+    handleSubmit,
+    handleChange,
+    isSubmitting,
+    values,
+  }: FormikProps<any>) => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          onChange={handleChange}
+          value={values.username}
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="password"
+          onChange={handleChange}
+          value={values.password}
+          type="password"
+
+        />
+        <br />
+        <Stack direction={"row"} spacing={2} sx={classes.buttons} >
+          <Button 
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={isSubmitting}
+          >
+            CREATE
+          </Button>
+          <Button
+            onClick={() => navigate("/login")}
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={isSubmitting}
+          >
+            Cancel
+          </Button>
+        </Stack>
+
+      </form>
+    );
+  };
+
   return (
     <>
-      <button onClick={() => navigate(-1)}>Back</button>
-    
-      <h1>Register</h1>
+      <Box sx={classes.root}>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 20 ,justifyContent:"center",display:'flex'} } color="text.secondary" gutterBottom >
+              <h3>Register</h3>
+            </Typography>
+            <Formik
+              onSubmit={(values, { setSubmitting }) => {
+                alert(JSON.stringify(values));
 
-   <form onSubmit={(e: React.FormEvent<Element>) =>{
-     e.preventDefault();
-     alert("Hi  "+ account.username)
-    }}>
-      <label htmlFor="">Username : </label>
-      <input type="text" 
-      name="username" 
-      id="username" 
-      onChange={(e)=>{console.log(e.target.value);
-        setAccount ({
-          ...account,
-          username: e.target.value,
-          
-        })
-      }}
-      
-      />
-      <br />
-      <label htmlFor="">Password : </label>
-        <input type="password" 
-        name="password" 
-        id="password" 
-        onChange={(e) => { console.log(e.target.value);
-          setAccount({
-            ...account,
-            password: e.target.value,
-          })
-        }} /> 
-        <br />
-      <button type="submit">Submit</button>
-<br />
-<br />
-      <span>
-          Debug:{JSON.stringify(account)}
-      </span>
-      
+                setTimeout(() => {
+                  setSubmitting(false);
+                }, 2000);
+              }}
+              initialValues={{ username: "", password: "" }}
+            >
+              {(props) => showFormV2(props)}
+            </Formik>
+          </CardContent>
+        </Card>
 
-   </form>
 
-     
 
+
+
+
+
+      </Box>
     </>
-  )
+  );
 };
 
 export default RegisterPage;
