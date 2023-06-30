444d3e634641f322b630d3057673966b46922cce
juon <juonjang@gmail.com>
2023-06-30 16:26:19 +0700

    ep32 redcers

--- src/actions/register.action.ts
+++ src/actions/register.action.ts
@@ -1,15 +1,37 @@
-import { REGISTER_FAILED, REGISTER_FETCHING, REGISTER_SUCCESS } from "../Constants";
+import { User } from "./../../../mishos/src/types/user.type";
+import {
+  REGISTER_FAILED,
+  REGISTER_FETCHING,
+  REGISTER_SUCCESS,
+  server,
+} from "../Constants";
+import { httpClient } from "../utils/httpclient";
 
 export const setRegisterFetchingToState = () => ({
   type: REGISTER_FETCHING,
-})
+});
 
-export const setRegisterSuccessToState = (payload:any) => ({
+export const setRegisterSuccessToState = (payload: any) => ({
   type: REGISTER_SUCCESS,
-  payload
-})
+  payload,
+});
 
 export const setRegisterFailedToState = () => ({
   type: REGISTER_FAILED,
-  
-})
+});
+
+export const register = (user: User) => {
+  return async (dispatch: any) => {
+    try {
+      // begin connection...
+      dispatch(setRegisterFailedToState);
+
+      //connect
+      const result = await httpClient.post(server.REGISTER_URL, user);
+      dispatch(setRegisterSuccessToState(result.data));
+    } catch (err) {
+      //error
+      dispatch(setRegisterFailedToState());
+    }
+  };
+};
